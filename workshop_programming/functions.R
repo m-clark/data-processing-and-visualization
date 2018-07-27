08e209cbe27b313131b0aec9d037fa97e17c37ff
Michael Clark <micl@umich.edu>
2018-07-27 09:05:38 -0400

    last content edits before workshop

---
+++ workshop_programming/functions.R
@@ -0,0 +1,159 @@
+## ----func_ex, eval=FALSE-------------------------------------------------
+## mean(myvar)
+## sd(myvar)
+## sum(is.na(myvar))
+
+## ----func_ex2------------------------------------------------------------
+my_summary <- function(myvar) {
+  data.frame(
+    mean = mean(myvar, na.rm = TRUE),
+    sd = sd(myvar, na.rm = TRUE),
+    N_missing = sum(is.na(myvar))
+    )
+}
+
+## ----func_ex3, echo=-1---------------------------------------------------
+mtcars = datasets::mtcars  # to undo previous factors
+my_summary(mtcars$mpg)
+
+mtcars2 = mtcars
+mtcars2$wt[c(3,10)] = NA
+my_summary(mtcars2$wt)
+
+## ----func_ex4------------------------------------------------------------
+my_summary <- function(myvar) {
+  data.frame(
+    mean = mean(myvar, na.rm = TRUE),
+    sd = sd(myvar, na.rm = TRUE),
+    N_total = length(myvar),
+    N_missing = sum(is.na(myvar))
+    )
+}
+
+## ----func_ex5------------------------------------------------------------
+my_summary(mtcars2$wt)
+
+## ----func_ex6------------------------------------------------------------
+mtcars2 %>% 
+  map_dfr(my_summary, .id = 'variable')
+
+## ----dry, eval=FALSE-----------------------------------------------------
+## good_mileage_displ_low_cyl_4  = if_else(cyl == 4 & displ < mean(displ) & hwy > 30, 'yes', 'no')
+## good_mileage_displ_low_cyl_6  = if_else(cyl == 6 & displ < mean(displ) & hwy > 30, 'yes', 'no')
+## good_mileage_displ_low_cyl_8  = if_else(cyl == 8 & displ < mean(displ) & hwy > 30, 'yes', 'no')
+## good_mileage_displ_high_cyl_4 = if_else(cyl == 4 & displ > mean(displ) & hwy > 30, 'yes', 'no')
+## good_mileage_displ_high_cyl_6 = if_else(cyl == 6 & displ > mean(displ) & hwy > 30, 'yes', 'no')
+## good_mileage_displ_high_cyl_8 = if_else(cyl == 8 & displ > mean(displ) & hwy > 30, 'yes', 'no')
+
+## ----mpgfunc-------------------------------------------------------------
+good_mileage <- function(cylinder = 4,
+                         mpg_cutoff = 30,
+                         displ_fun = mean,
+                         displ_low = TRUE,
+                         cls = 'compact') {
+  
+  if (displ_low == TRUE) {
+    result = mpg %>%
+      filter(cyl == cylinder,
+             displ <= displ_fun(displ),
+             hwy >= mpg_cutoff,
+             class == cls)
+  }
+  else {
+    result = mpg %>%
+      filter(cyl == cylinder,
+             displ >= displ_fun(displ),
+             hwy >= mpg_cutoff,
+             class == cls)
+  }
+  
+  result
+}
+
+## if (Math.random() < 0.5) {
+
+## if x == 2:
+
+## ----mpgfunc_demo--------------------------------------------------------
+good_mileage(mpg_cutoff = 40)
+
+good_mileage(cylinder = 8, mpg_cutoff = 15, displ_low = F, cls='suv')
+
+## ----mpgfunc_extend, echo=1:6--------------------------------------------
+good_mileage <- function(cylinder = 4,
+                         mpg_cutoff = 30,
+                         displ_fun = mean,
+                         displ_low = TRUE,
+                         cls = 'compact',
+                         yr = 2008) {
+  
+  if (displ_low) {
+    result = mpg %>%
+    filter(cyl == cylinder,
+           displ <= displ_fun(displ),
+           hwy >= mpg_cutoff,
+           class == cls,
+           year == yr)
+  }
+  else {
+    result = mpg %>%
+    filter(cyl == cylinder,
+           displ >= displ_fun(displ),
+           hwy >= mpg_cutoff,
+           class == cls,
+           year == yr)
+  }
+  
+  result
+}
+
+## ----mpgfunc_extend_demo-------------------------------------------------
+good_mileage(cylinder = 8, 
+             mpg_cutoff = 19, 
+             displ_low = F, 
+             cls='suv', 
+             yr = 2008)
+
+## ----lambda, eval=FALSE--------------------------------------------------
+## apply(mtcars, 2, sd)
+## apply(mtcars, 2, function(x) sd(x))
+
+## ----lambda_ex, cache.rebuild=T------------------------------------------
+# some variables have a mad = 0, and so return Inf, NaN
+# apply(mtcars, 2, function(x) (x - median(x))/mad(x)) %>% 
+#   head()
+
+mtcars %>%
+  map_df(function(x) (x - median(x))/mad(x))
+
+## ----wf_ex1, eval=FALSE--------------------------------------------------
+## log_sum <- function(a, b) {
+##   ?
+## }
+## 
+
+## ----wf_ex1b, eval=FALSE-------------------------------------------------
+## log_sum <- function(a, b) {
+##   #
+##   #
+##   #
+## 
+##   if (?) {
+##     stop('Your message here.')
+##   }
+##   else {
+##     return(your_log_sum_object)
+##   }
+## }
+## 
+
+## ----wf_ex2, eval=FALSE--------------------------------------------------
+## set.seed(123)  # so you get the exact same 'random' result
+## x <- rnorm(10)
+## if_else(x < 0, "negative", "positive")
+
+## ----wf_ex2b, eval=FALSE-------------------------------------------------
+## pos_neg <- function(?) {
+##   ?
+## }
+
---
+++ workshop_programming/programming.R
@@ -0,0 +1,70 @@
+## ----progsetup, include=FALSE, eval=TRUE, cache=FALSE--------------------
+knitr::opts_chunk$set(eval=T, echo=T)
+
+## ----view, eval=FALSE----------------------------------------------------
+## View(diamonds)
+
+## ----str-----------------------------------------------------------------
+str(diamonds)
+glimpse(diamonds)
+
+## ----lm_object-----------------------------------------------------------
+lm_mod = lm(mpg ~ ., data=mtcars)
+str(lm_mod, 0)
+str(lm_mod, 1)
+
+## ----lm_object2----------------------------------------------------------
+lm_mod$coefficients
+lm_mod$model %>% 
+  head()
+
+## ----lm_mod_summary------------------------------------------------------
+summary(lm_mod)
+
+## ----lm_mod_summary_obj--------------------------------------------------
+lm_mod_summary = summary(lm_mod)
+str(lm_mod_summary)
+
+## ----lm_mod_summary_table------------------------------------------------
+lm_mod_summary$coefficients %>%
+  kableExtra::kable(digits = 2)
+
+## ----summary-------------------------------------------------------------
+summary(diamonds)
+summary(diamonds$clarity)
+summary(lm_mod)
+summary(lm_mod_summary)
+
+## ----methods_summary-----------------------------------------------------
+methods('summary')
+
+## ----methods_brms--------------------------------------------------------
+library(brms)
+methods(class = 'brmsfit')
+
+## ----s4_example----------------------------------------------------------
+car_matrix = mtcars %>% 
+  as.matrix() %>%       # convert from df to matrix
+  Matrix::Matrix()      # convert to Matrix class (S4)
+
+typeof(car_matrix)
+str(car_matrix)
+
+## ----lm_func-------------------------------------------------------------
+str(lm)
+
+## ----lm_func2, eval=2, echo=1--------------------------------------------
+lm
+head(lm, 20)
+
+## ----help, eval=FALSE----------------------------------------------------
+## sample(?)
+
+## ----help2, error=TRUE---------------------------------------------------
+nums = 1:5
+sample(nums, 10)
+
+## ----tibble_inspect------------------------------------------------------
+library(dplyr)
+?starwars
+
