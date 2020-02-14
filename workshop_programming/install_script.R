43d7f4b266c649341192868305dcf103b1b1b589
Michael <micl@umich.edu>
2020-02-14 11:11:41 -0500

    g2g for workshop

---
+++ workshop_programming/install_script.R
@@ -0,0 +1,23 @@
+# Install libraries -------------------------------------------------------
+
+install.packages(
+  c(
+    'tidyverse',
+    'haven',       # not required for workshop but potentially useful
+    'data.table'
+  )
+)
+
+
+# to run interactive exercises (not required!):
+
+install.packages('learnr')
+
+
+
+# Load libraries ----------------------------------------------------------
+
+
+library(tidyverse)      # loads dplyr, ggplot2, purrr, etc.
+
+# load others as needed
